24a5beab70744026357103d55d2e82733537c7f7
bkhj17 <72788759+bkhj17@users.noreply.github.com>
2023-02-18 15:56:42 +0900

    로그 누락 해결

---
+++ DirectX2D/_Pokemon/Log/LogManager.cpp
@@ -0,0 +1,14 @@
+#include "Framework.h"
+
+void LogManager::InsertLog(string log)
+{
+	logs.push_back(log);
+	Observer::Get()->ExecuteEvent("ShowLog");
+}
+
+void LogManager::GetRenderLog(OUT vector<string>& renderLogs)
+{
+	//뒤에서부터 일정 개수까지만 출력
+	for (int i = max((int)logs.size() - MAX_RENDER, 0); i < logs.size(); i++)
+		renderLogs.push_back(logs[i]);
+}
